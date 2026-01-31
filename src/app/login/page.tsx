16e6c23c70e7ea8e2f38a7d0e712f8e5fc621660
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-31 23:07:24 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    AlertDialogCancel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -26,6 +26,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   AlertDialog,
+  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
